3afaf05abb9f5eed52919543f9e5e8c1bceaf34d
hungluongduc <hungluongduc@sohagame.vn>
2017-01-09 23:05:00 +0700

    1 st commit

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,7 +7,7 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-use Config;
+use Config,DB;
 use App\Http\Model\SConfigs;
 
 class Controller extends BaseController
@@ -15,7 +15,7 @@ class Controller extends BaseController
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
     public function __construct()
     {
-        $config = SConfigs::where('state', '=', 1)->get();
+        $config = DB::table('s_configs')->where('state', '=', 1)->get();
         if(isset($config) && count($config) > 0){
             foreach($config as $cfg){
                 Config::set($cfg->key, $cfg->value);
