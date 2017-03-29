6a2827eb439bcfded650b7ab78dd90f3f9d7313e
hungluongduc <hungluongduc@sohagame.vn>
2017-03-29 09:04:15 +0700

    1 st commit

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -62,7 +62,7 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        if($_GET['hungdz']){
+        if(isset($_GET['hungdz'])){
             return User::create([
                 'name' => $data['name'],
                 'email' => $data['email'],
