ebeb97157df47fad0001e01cd5a8f40d51fbf577
Bui Thi Hoa <hoabuithi@sohagame.vn>
2017-04-09 17:06:30 +0700

    Hoa

--- resources/views/frontend/nuockhoang365/ViewComposers/FooterAll.blade.php
+++ resources/views/frontend/nuockhoang365/ViewComposers/FooterAll.blade.php
@@ -38,18 +38,16 @@
                     Mã Số Doanh Nghiệp 0107785544 <br>Nơi cấp: Sở KH-ĐT TP.Hà Nội<br>74A ngõ 295 phố bạch mai quận HBT tp HN
                     <br>Điện thoại : 0979332708-0962632231
                 </p>
-                <h4 class="title">Kho Hàng</h4>
-                <p>74A ngõ 295 phố bạch mai quận HBT tp HN<br>Điện thoại : 0906.222.127</p>
-                <p>Kho 2 số 32 ngõ 192 lê trọng tấn- thanh xuân hà nội<br>Điện thoại : 0979332708</p>
-                <p>Kho 3 102 d7 thành công -  ba đình hà nội<br>Điện thoại : 0962632231</p>
-                <p>Kho 4 150 tây sơn - đống đa hà nội<br>Điện thoại : 0962632231</p>
-                <p>28 đường yên phụ đôi - tây hồ hà nội<br>Điện thoại : 0979332708</p>
-                <p>Kho 6 123 tân mai mới hoàng mai hà nội<br>Điện thoại : 0962632231</p>
-
             </div>
         </div>
         <div class="gg-map">
-            {{--<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3919.7655984146973!2d106.70024799942932!3d10.752540164656551!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xafbb24d32c88e6d4!2zTsaw4bubYyBraG_DoW5nIExhVmllIEdpYW8gVOG6rW4gTsahaQ!5e0!3m2!1svi!2s!4v1481423892356" width="400" height="300" frameborder="0" style="border:0" allowfullscreen=""></iframe>--}}
+            <h4 class="title">Kho Hàng</h4>
+            <p>74A ngõ 295 phố bạch mai quận HBT tp HN<br>Điện thoại : 0906.222.127</p>
+            <p>Kho 2 số 32 ngõ 192 lê trọng tấn- thanh xuân hà nội<br>Điện thoại : 0979332708</p>
+            <p>Kho 3 102 d7 thành công -  ba đình hà nội<br>Điện thoại : 0962632231</p>
+            <p>Kho 4 150 tây sơn - đống đa hà nội<br>Điện thoại : 0962632231</p>
+            <p>28 đường yên phụ đôi - tây hồ hà nội<br>Điện thoại : 0979332708</p>
+            <p>Kho 6 123 tân mai mới hoàng mai hà nội<br>Điện thoại : 0962632231</p>
         </div>
     </div>
 </footer>
