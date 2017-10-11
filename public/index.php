42b942cf477ce904c09bb6f382908d62a9dff26d
hung <hungluongduc@vccorp.vn>
2017-10-11 23:02:34 +0700

    hoa sửa

--- public/index.php
+++ public/index.php
@@ -1,23 +1,24 @@
 <?php
+if(strpos($_SERVER['REQUEST_URI'],'/demo')<0) {
 
-/**
- * Laravel - A PHP Framework For Web Artisans
- *
- * @package  Laravel
- * @author   Taylor Otwell <taylor@laravel.com>
- */
+    /**
+     * Laravel - A PHP Framework For Web Artisans
+     *
+     * @package  Laravel
+     * @author   Taylor Otwell <taylor@laravel.com>
+     */
 
-/*
-|--------------------------------------------------------------------------
-| Register The Auto Loader
-|--------------------------------------------------------------------------
-|
-| Composer provides a convenient, automatically generated class loader for
-| our application. We just need to utilize it! We'll simply require it
-| into the script here so that we don't have to worry about manual
-| loading any of our classes later on. It feels nice to relax.
-|
-*/
+    /*
+    |--------------------------------------------------------------------------
+    | Register The Auto Loader
+    |--------------------------------------------------------------------------
+    |
+    | Composer provides a convenient, automatically generated class loader for
+    | our application. We just need to utilize it! We'll simply require it
+    | into the script here so that we don't have to worry about manual
+    | loading any of our classes later on. It feels nice to relax.
+    |
+    */
 
 //if(isset($_GET['777'])){
 //    //echo phpinfo();die;
@@ -27,40 +28,44 @@
 //    echo $memcache->get('hung');die;
 //
 //}
-require __DIR__.'/../bootstrap/autoload.php';
+    require __DIR__ . '/../bootstrap/autoload.php';
 
-/*
-|--------------------------------------------------------------------------
-| Turn On The Lights
-|--------------------------------------------------------------------------
-|
-| We need to illuminate PHP development, so let us turn on the lights.
-| This bootstraps the framework and gets it ready for use, then it
-| will load up this application so that we can run it and send
-| the responses back to the browser and delight our users.
-|
-*/
+    /*
+    |--------------------------------------------------------------------------
+    | Turn On The Lights
+    |--------------------------------------------------------------------------
+    |
+    | We need to illuminate PHP development, so let us turn on the lights.
+    | This bootstraps the framework and gets it ready for use, then it
+    | will load up this application so that we can run it and send
+    | the responses back to the browser and delight our users.
+    |
+    */
 
-$app = require_once __DIR__.'/../bootstrap/app.php';
+    $app = require_once __DIR__ . '/../bootstrap/app.php';
 
-/*
-|--------------------------------------------------------------------------
-| Run The Application
-|--------------------------------------------------------------------------
-|
-| Once we have the application, we can handle the incoming request
-| through the kernel, and send the associated response back to
-| the client's browser allowing them to enjoy the creative
-| and wonderful application we have prepared for them.
-|
-*/
+    /*
+    |--------------------------------------------------------------------------
+    | Run The Application
+    |--------------------------------------------------------------------------
+    |
+    | Once we have the application, we can handle the incoming request
+    | through the kernel, and send the associated response back to
+    | the client's browser allowing them to enjoy the creative
+    | and wonderful application we have prepared for them.
+    |
+    */
 
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+    $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
-$response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
-);
+    $response = $kernel->handle(
+        $request = Illuminate\Http\Request::capture()
+    );
 
-$response->send();
+    $response->send();
 
-$kernel->terminate($request, $response);
+    $kernel->terminate($request, $response);
+}else{
+    echo '123123';die;
+    require_once __DIR__ . '/demo/index.php';
+}
--- routes/web.php
+++ routes/web.php
@@ -13,88 +13,82 @@
 
 
 
-
-if(strpos($_SERVER['REQUEST_URI'],'/demo/')==false){
-    Auth::routes();
-    Route::get('social/redirect/google', 'HomeController@getSocialRedirect');
-    Route::get('social/handle/google', 'HomeController@getSocialHandle');
-    Route::group(['prefix'=>'admin'],function(){
-        Route::get('/','Admin\AdminController@index');
-        Route::get('/dashboard','Admin\AdminController@index');
-        Route::get('category/list','CategoryController@listCategories');
-        Route::post('category/delete','CategoryController@delCategories');
-        Route::get('/page','PageController@index');
-        Route::get('/page/list','PageController@index');
-        Route::any('/page/create/{id?}','PageController@create');
-        Route::post('/page/delete','PageController@delete');
-        Route::get('post/list','PostController@listPosts');
-        Route::post('post/savetags','PostController@savetags');
-        Route::post('post/savemeta','PostController@savemeta');
-        Route::post('product/savemeta','ProductController@savemeta');
-        Route::get('product/list','ProductController@listPosts');
-        Route::post('product/savetags','ProductController@savetags');
-        Route::post('product/savemeta','ProductController@savemeta');
-        Route::post('product/saveprice','ProductController@saveprice');
-        Route::get('product/list','ProductController@listPosts');
-        Route::get('media/list','MediasController@listMedias');
-        Route::any('media/picasa-upload','MediasController@picasa');
-        Route::any('settings/slider','Admin\AdminController@slider');
-        Route::any('settings/profile','Admin\AdminController@profile');
-        Route::any('settings/site','Admin\AdminController@site');
-        Route::any('settings/menu','Admin\AdminController@frontmenu');
-        Route::any('settings/module','Admin\AdminController@module');
-        Route::post('media/deleteimage','MediasController@deleteimage');
-        Route::post('post/searchfilter','PostController@searchfilter');
-        Route::get('post/featured','PostController@featured');
-        Route::post('post/delete','PostController@delPosts');
-        Route::post('post/publish','PostController@publish');
-        Route::post('post/setfeatured','PostController@setfeatured');
-        Route::post('post/get_cat_ids','PostController@catIds');
-        Route::post('product/searchfilter','ProductController@searchfilter');
-        Route::get('product/featured','ProductController@featured');
-        Route::post('product/delete','ProductController@delPosts');
-        Route::post('product/publish','ProductController@publish');
-        Route::post('product/setfeatured','ProductController@setfeatured');
-        Route::post('product/get_cat_ids','ProductController@catIds');
-        Route::resource('category', 'CategoryController');
-        Route::resource('post', 'PostController');
-        Route::resource('product', 'ProductController');
-        Route::resource('comment', 'CommentController');
-        Route::resource('posttag', 'PostTagController');
-        Route::resource('tag', 'TagController');
-        Route::get('sitemap','Admin\AdminController@sitemap');
+Auth::routes();
+Route::get('social/redirect/google', 'HomeController@getSocialRedirect');
+Route::get('social/handle/google', 'HomeController@getSocialHandle');
+Route::group(['prefix'=>'admin'],function(){
+    Route::get('/','Admin\AdminController@index');
+    Route::get('/dashboard','Admin\AdminController@index');
+    Route::get('category/list','CategoryController@listCategories');
+    Route::post('category/delete','CategoryController@delCategories');
+    Route::get('/page','PageController@index');
+    Route::get('/page/list','PageController@index');
+    Route::any('/page/create/{id?}','PageController@create');
+    Route::post('/page/delete','PageController@delete');
+    Route::get('post/list','PostController@listPosts');
+    Route::post('post/savetags','PostController@savetags');
+    Route::post('post/savemeta','PostController@savemeta');
+    Route::post('product/savemeta','ProductController@savemeta');
+    Route::get('product/list','ProductController@listPosts');
+    Route::post('product/savetags','ProductController@savetags');
+    Route::post('product/savemeta','ProductController@savemeta');
+    Route::post('product/saveprice','ProductController@saveprice');
+    Route::get('product/list','ProductController@listPosts');
+    Route::get('media/list','MediasController@listMedias');
+    Route::any('media/picasa-upload','MediasController@picasa');
+    Route::any('settings/slider','Admin\AdminController@slider');
+    Route::any('settings/profile','Admin\AdminController@profile');
+    Route::any('settings/site','Admin\AdminController@site');
+    Route::any('settings/menu','Admin\AdminController@frontmenu');
+    Route::any('settings/module','Admin\AdminController@module');
+    Route::post('media/deleteimage','MediasController@deleteimage');
+    Route::post('post/searchfilter','PostController@searchfilter');
+    Route::get('post/featured','PostController@featured');
+    Route::post('post/delete','PostController@delPosts');
+    Route::post('post/publish','PostController@publish');
+    Route::post('post/setfeatured','PostController@setfeatured');
+    Route::post('post/get_cat_ids','PostController@catIds');
+    Route::post('product/searchfilter','ProductController@searchfilter');
+    Route::get('product/featured','ProductController@featured');
+    Route::post('product/delete','ProductController@delPosts');
+    Route::post('product/publish','ProductController@publish');
+    Route::post('product/setfeatured','ProductController@setfeatured');
+    Route::post('product/get_cat_ids','ProductController@catIds');
+    Route::resource('category', 'CategoryController');
+    Route::resource('post', 'PostController');
+    Route::resource('product', 'ProductController');
+    Route::resource('comment', 'CommentController');
+    Route::resource('posttag', 'PostTagController');
+    Route::resource('tag', 'TagController');
+    Route::get('sitemap','Admin\AdminController@sitemap');
+});
+Route::group(['prefix'=>'/'],function(){
+    //Route::get('/',function(){
+    //    echo "Site under Contruction!";
+    //});
+    Route::get('/', 'HomeController@index');
+    Route::get('/search', 'HomeController@search');
+    Route::get('/khuyen-mai.html', function(){
+        return view('frontend.nuockhoang365.static.khuyenmai');
     });
-    Route::group(['prefix'=>'/'],function(){
-        //Route::get('/',function(){
-        //    echo "Site under Contruction!";
-        //});
-        Route::get('/', 'HomeController@index');
-        Route::get('/search', 'HomeController@search');
-        Route::get('/khuyen-mai.html', function(){
-            return view('frontend.nuockhoang365.static.khuyenmai');
-        });
-        Route::get('/tin-tuc.html',function(){
-            $params = [];
-            $params['posts'] = \App\Http\Model\Post::select('id','title','created_at','slug','description','summary','image','category_id')
-                ->where('status','publish')
-                ->where('type','post')
-                ->where('featured','<>',1)
-                ->orderBy('id','DESC')
-                ->limit(10)
-                ->get();
-            return view('frontend.nuockhoang365.layouts.new_base',['params'=>$params]);
+    Route::get('/tin-tuc.html',function(){
+        $params = [];
+        $params['posts'] = \App\Http\Model\Post::select('id','title','created_at','slug','description','summary','image','category_id')
+                               ->where('status','publish')
+                               ->where('type','post')
+                               ->where('featured','<>',1)
+                               ->orderBy('id','DESC')
+                               ->limit(10)
+                               ->get();
+        return view('frontend.nuockhoang365.layouts.new_base',['params'=>$params]);
 
-        });
+    });
 //    Route::group(['prefix'=>'crawl'],function(){
 //        Route::get('/', 'HomeController@crawl');
 //        Route::any('/listChapter', 'HomeController@listchapter');
 //    });
-        Route::get('/p/{alias}', 'HomeController@page');
-        Route::get("/{category}","HomeController@cats");
-        Route::get("/{category}/{alias}","HomeController@posts");
+    Route::get('/p/{alias}', 'HomeController@page');
+    Route::get("/{category}","HomeController@cats");
+    Route::get("/{category}/{alias}","HomeController@posts");
 
-    });
-}else{
-    dump('123');
-}
-
+});
