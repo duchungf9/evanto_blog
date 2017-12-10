c3934b533fbac6374a1b402889937ed83443270b
hung <hungluongduc@vccorp.vn>
2017-12-10 20:09:55 +0700

    hoa sửa

--- app/Http/ViewComposers/nuockhoang365/HomeMain.php
+++ app/Http/ViewComposers/nuockhoang365/HomeMain.php
@@ -7,73 +7,96 @@ use App\Http\Model\SConfigs;
 use App\Http\Model\Post;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\View\View;
-use DB,Cache;
+use DB, Cache;
+
 class HomeMain
 {
+    protected $fixCung = [
+        'nuoc-lavie' => [
+            'image' => 'http://nuockhoang24h.com/Pictures/Banner/lavie-banner.jpg',
+            'description' => 'Với người tiêu dùng Việt Nam, nước khoáng LaVie đã trở nên vô cùng quen thuộc, có mặt khắp mọi nơi. LaVie được khai thác từ nguồn nước nhiên tinh khiết nhất, được bổ sung rất nhiều khoáng chất có lợi cho cơ thể trong cuộc sống năng động hàng ngày.
+Lavie được đóng chai tại nguồn với công nghệ hiện đại của Nestlé Waters, không sử dụng hóa chất trong quy trình xử lý, có công bố chất lượng sản phẩm theo tiêu chuẩn Việt Nam.',
+            'image2' => 'http://nuockhoang24h.com/Pictures/Banner/lavie-banner2.jpg',
+        ],
+        'nuoc-miru' => [
+            'image' => 'http://nuockhoang24h.com/Pictures/Banner/Miru-banner-left.jpg',
+            'description' => 'Nước tính khiết Miru sản phẩm của công ty TNHH nước giải khát Vạn Xuân.
+Với dây chuyền sản xuất tự động khép kín, sản phẩm nước tinh khiết Miru luôn đảm bảo chất lượng ổn định từ sản phẩm đầu tiên đến sản phẩm cuối cùng. Tất cả bao bì nước uống Miru đều được thanh trùng trước khi đưa vào sản xuất.
+Hệ thống KCS đang được áp dụng tại công ty chúng tôi theo chứng chỉ quản lý chất lượng ISO 9001:2000 và ISO 22000 Sản phẩm của chúng tôi đa dạng về chủng loại, kích cỡ phù hợp với mọi nhu cầu.',
+            'image2' => 'http://nuockhoang24h.com/Pictures/Banner/Miru-banner-right.jpg',
+        ],
+        'nuoc-vinawa'=>[
+            'image' => 'http://vinawa.com.vn/vnt_upload/product/11_2013/thumbs/160_crop__MG_6295_1.jpg',
+            'description' => 'Nước uống tinh khiết Vinawa là một trong những dòng sản phẩm chất lượng cao của Tổng Công ty Thuốc lá Việt Nam - Vinataba, được sản xuất gia công tại Công ty CP Thương Mại Hàng Không Miền Nam - SATCOtrên dây chuyền sản xuất hiện đại như công nghệ thẩm thấu ngược, sử dụng màng lọc RO, xử lý nước bằng tia cực tím và Ozon công nghệ do hãng Waterman (Mỹ) chuyển giao.
+ 
+Nước uống tinh khiết VINAWA, tuân thủ nghiêm ngặt theo tiêu chuẩn về công nghệ và chất lượng của cơ quan Quản lý Không gian, Hàng không quốc gia HOA KỲ - NASA.
+ 
+Nước uống tinh khiết VINAWA đảm bảo chất lượng cao. Vì sức khỏe và sự hài lòng của Quý khách hàng chính là phương châm và động lực phát triển của Chúng tôi.',
+            'image2' =>'http://vinawa.com.vn/vnt_upload/product/11_2013/thumbs/160_crop_MG_1643_resized_2.jpg'
+        ]
+    ];
 
     public function __construct()
     {
         // Dependencies automatically resolved by service container...
+
     }
 
     /**
      * Bind data to the view.
      *
-     * @param  View  $view
+     * @param  View $view
      * @return void
      */
     public function compose(View $view)
     {
         $categorySLug = null;
-        if(isset($_SERVER['REDIRECT_URL']) or isset($_SERVER['REQUEST_URI'])){
-            $href = (isset($_SERVER['REDIRECT_URL']))?$_SERVER['REDIRECT_URL']:$_SERVER['REQUEST_URI'];
-            $categorySLug = str_replace("/","",$href);
-            $categorySLug = str_replace("/","",$href);
-            $category = Category::where('slug',$categorySLug)->first();
+        if (isset($_SERVER['REDIRECT_URL']) or isset($_SERVER['REQUEST_URI'])) {
+            $href = (isset($_SERVER['REDIRECT_URL'])) ? $_SERVER['REDIRECT_URL'] : $_SERVER['REQUEST_URI'];
+            $categorySLug = str_replace("/", "", $href);
+            $categorySLug = str_replace("/", "", $href);
+            $category = Category::where('slug', $categorySLug)->first();
         }
-        if(!isset($category)){
+        if (!isset($category)) {
             $params = [];
-//            $params['featured_posts'] = Post::select('blog_posts.id','blog_posts.title','blog_posts.created_at','blog_posts.slug','blog_posts.description','blog_posts.summary','blog_posts.image','blog_posts.json_params','categories.name','categories.id as cat_id','categories.slug as cat_slug')
-//                                            ->join('categories','categories.id','=','blog_posts.category_id')
-//                                            ->where('blog_posts.status','publish')
-//                                            ->where('blog_posts.featured','=',1)
-//                                            ->where('blog_posts.type','=','product')
-//                                            ->orderBy('blog_posts.category_id','ASC')
-//                                            ->orderBy('blog_posts.id','ASC')
-//                                            ->get();
-            $menu = SConfigs::where('key','app.menu')->first();
-            if(!$menu){$menu=[];}else{$menu=unserialize($menu->value);}
-            $params['categories'] = Category::orderBy('id','desc')->whereIn('id',$menu)->limit(10)->get();
+            $menu = SConfigs::where('key', 'app.menu')->first();
+            if (!$menu) {
+                $menu = [];
+            } else {
+                $menu = unserialize($menu->value);
+            }
+            $params['categories'] = Category::orderBy('id', 'desc')->whereIn('id', $menu)->limit(10)->get();
+            foreach($params['categories'] as $key=>$value)
+            {
+                foreach($this->fixCung as $key=>$valueX){
+                    if($value->slug == $key){
+                        $value->image = $valueX['image'];
+                        $value->image2 = $valueX['image2'];
+                        $value->mota = $valueX['description'];
+                    }
+                }
 
-//            foreach($params['categories'] as $cat){
-////                $cat->posts = Post::select('id','title','created_at','json_params','slug','description','summary','image')
-////                                  ->where('category_id',$cat->id)
-////                                  ->where('type','product')
-////                                  ->orderBy('id','DESC')->get();
-//            }
-//            $a = DB::table('blog_posts')->where('type','product')->where('category_id',22)->get();dd($a);
-//            dd($params);
+            }
             $view->with('params', $params);
-        }else{
+        } else {
             $params = [];
-            $params['featured_posts'] = Post::select('id','title','created_at','slug','description','summary','image','json_params')
-                                            ->where('status','publish')
-                                            ->where('category_id',$category->id)
-                                            ->where('featured','=',1)
-                                            ->where('type','=','product')
-                                            ->orderBy('id','ASC')
-                //->limit(4)
-                                            ->get();
+            $params['featured_posts'] = Post::select('id', 'title', 'created_at', 'slug', 'description', 'summary', 'image', 'json_params')
+                ->where('status', 'publish')
+                ->where('category_id', $category->id)
+                ->where('featured', '=', 1)
+                ->where('type', '=', 'product')
+                ->orderBy('id', 'ASC')
+                ->get();
             $params['category'] = $category;
-            $catPhukien = Category::where('slug','phu-kien')->first();
-            if($catPhukien==null){
+            $catPhukien = Category::where('slug', 'phu-kien')->first();
+            if ($catPhukien == null) {
                 $catPhukien = new Category();
                 $catPhukien->name = "Phụ Kiện";
                 $catPhukien->slug = "phu-kien";
                 $catPhukien->description = "Phụ Kiện";
                 $catPhukien->save();
             }
-            $catPhukien->products = Post::where('category_id',$catPhukien->id)->where('status','publish')->orderBy('id','ASC')->get();
+            $catPhukien->products = Post::where('category_id', $catPhukien->id)->where('status', 'publish')->orderBy('id', 'ASC')->get();
             $params['phu-kien'] = $catPhukien;
             $view->with('params', $params);
         }
--- resources/views/frontend/nuockhoang365/ViewComposers/HomeMain.blade.php
+++ resources/views/frontend/nuockhoang365/ViewComposers/HomeMain.blade.php
@@ -1,79 +1,91 @@
 <section class="fixCen" id="content">
-<div class="showProduct">
-    @if(isset($params['featured_posts']))
-        @foreach($params['featured_posts'] as $post)
-            <?php
+    <div class="showProduct">
+        @if(isset($params['featured_posts']))
+            @foreach($params['featured_posts'] as $post)
+                <?php
                 $json_params = json_decode($post->json_params);
-//                dump($json_params->price);die;
-                $price = (isset($json_params->price)?$json_params->price:0);
+                //                dump($json_params->price);die;
+                $price = (isset($json_params->price) ? $json_params->price : 0);
 
-                $period = explode("/",$price);$period = isset($period[1])?$period[1]:"bình";
-                $price = explode(" ",$price); $price = isset($price[0])?$price[0]:0;
-            ?>
-            <div class="product">
-                <div class="image" style="background-image: url({{$post->image}})">
-                    <img src="{{$post->image}}"
-                         alt="{{$post->title}}" width="470" height="470">
+                $period = explode("/", $price);$period = isset($period[1]) ? $period[1] : "bình";
+                $price = explode(" ", $price); $price = isset($price[0]) ? $price[0] : 0;
+                ?>
+                <div class="product">
+                    <div class="image" style="background-image: url({{$post->image}})">
+                        <img src="{{$post->image}}"
+                             alt="{{$post->title}}" width="470" height="470">
+                    </div>
+                    <h2>{{$post->title}}</h2>
+                    <div class="price"><span>{{$price}}</span><sup class="currency">đ</sup><sup
+                                class="period">/{{$period}}</sup></div>
+                    <div class="hr_color"></div>
+                    <div class="subtitle">{{$post->summary}}</div>
                 </div>
-                <h2>{{$post->title}}</h2>
-                <div class="price"><span>{{$price}}</span><sup class="currency">đ</sup><sup class="period">/{{$period}}</sup></div>
-                <div class="hr_color"></div>
-                <div class="subtitle">{{$post->summary}}</div>
-            </div>
-        @endforeach
-    @endif
-</div>
+            @endforeach
+        @endif
+    </div>
 
     @if(isset($params['categories']))
         <?php $params['categories'] = ($params['categories'])->sortBy('id'); ?>
         @foreach($params['categories'] as $key=>$cat)
             <h2 class="pk-title"><i></i><span>{{$cat->name}}</span><i></i></h2>
+            @if(isset($cat->mota))
+            <div class="intro intro{{$key}}">
+                <div class="left"> <img src="{!! $cat->image !!}" title="" alt=""></div>
+                <div class="center">
+                    {!! $cat->mota !!}
+                </div>
+                <div class="right">
+                    <img src="{!! $cat->image2 !!}" title="" alt="">
+                </div>
+            </div>
+            @endif
             <div class="show-equiment">
                 @if(isset($cat->posts))
                     @foreach($cat->posts as $post)
                         @if($post->type=='product')
-                        <?php
-                        $json_params = json_decode($post->json_params);
-                        //                dump($json_params->price);die;
-                        $price = (isset($json_params->price)?$json_params->price:0);
+                            <?php
+                            $json_params = json_decode($post->json_params);
+                            //                dump($json_params->price);die;
+                            $price = (isset($json_params->price) ? $json_params->price : 0);
 
-                        $period = explode("/",$price);$period = isset($period[1])?$period[1]:"bình";
-                        $price = explode(" ",$price); $price = isset($price[0])?$price[0]:0;
+                            $period = explode("/", $price);$period = isset($period[1]) ? $period[1] : "bình";
+                            $price = explode(" ", $price); $price = isset($price[0]) ? $price[0] : 0;
 
-                        ?>
-                        <div class="product">
-                            <div class="image" style="background-image: url({{$post->image}})">
-                                <img src="{{$post->image}}" alt="{{$post->title}}" width="488" height="488">
+                            ?>
+                            <div class="product">
+                                <div class="image" style="background-image: url({{$post->image}})">
+                                    <img src="{{$post->image}}" alt="{{$post->title}}" width="488" height="488">
+                                </div>
+                                <h2>{{$post->title}}</h2>
+                                <div class="price"><span>{{$price}}</span><sup class="currency">đ</sup><sup
+                                            class="period">/ {{$period}}</sup>
+                                </div>
+                                <div class="hr_color"></div>
+                                <div class="subtitle">{{$post->summary}}</div>
                             </div>
-                            <h2>{{$post->title}}</h2>
-                            <div class="price"><span>{{$price}}</span><sup class="currency">đ</sup><sup class="period">/ {{$period}}</sup>
-                            </div>
-                            <div class="hr_color"></div>
-                            <div class="subtitle">{{$post->summary}}</div>
-                        </div>
-                        {{--<article class="aNewCatMiniBig">--}}
-                        {{--<a href="{{URL::to('/').'/'.$cat->slug.'/'.$post->slug}}" class="imgThumb"><img src="{{$post->image}}" alt="{{$post->title}}"></a>--}}
-                        {{--<h3 class="rs"><a href="{{URL::to('/').'/'.$cat->slug.'/'.$post->slug}}">{{$post->description}}</a></h3>--}}
-                        {{--</article>--}}
+                            {{--<article class="aNewCatMiniBig">--}}
+                            {{--<a href="{{URL::to('/').'/'.$cat->slug.'/'.$post->slug}}" class="imgThumb"><img src="{{$post->image}}" alt="{{$post->title}}"></a>--}}
+                            {{--<h3 class="rs"><a href="{{URL::to('/').'/'.$cat->slug.'/'.$post->slug}}">{{$post->description}}</a></h3>--}}
+                            {{--</article>--}}
                         @endif
                     @endforeach
                 @endif
-
             </div>
         @endforeach
     @endif
     @if(isset($params['phu-kien']))
-    <h2 class="pk-title"><i></i><span>Phụ kiện</span><i></i></h2>
-    <div class="show-equiment">
+        <h2 class="pk-title"><i></i><span>Phụ kiện</span><i></i></h2>
+        <div class="show-equiment">
             @if(isset($params['phu-kien']->products))
                 @foreach($params['phu-kien']->products as $key=>$value)
                     <?php
                     $json_params = json_decode($value->json_params);
                     //                dump($json_params->price);die;
-                    $price = (isset($json_params->price)?$json_params->price:0);
+                    $price = (isset($json_params->price) ? $json_params->price : 0);
 
-                    $period = explode("/",$price);$period = isset($period[1])?$period[1]:"bình";
-                    $price = explode(" ",$price); $price = isset($price[0])?$price[0]:0;
+                    $period = explode("/", $price);$period = isset($period[1]) ? $period[1] : "bình";
+                    $price = explode(" ", $price); $price = isset($price[0]) ? $price[0] : 0;
 
                     ?>
                     <div class="product">
@@ -82,13 +94,14 @@
                                  width="488" height="488">
                         </div>
                         <h2>{{$value->title}}</h2>
-                        <div class="price"><span>{{$price}}</span><sup class="currency">đ</sup><sup class="period">/ {{$period}}</sup>
+                        <div class="price"><span>{{$price}}</span><sup class="currency">đ</sup><sup
+                                    class="period">/ {{$period}}</sup>
                         </div>
                         <div class="hr_color"></div>
                         <div class="subtitle">{{$value->summary}}</div>
                     </div>
                 @endforeach
             @endif
-    </div>
+        </div>
     @endif
 </section>
